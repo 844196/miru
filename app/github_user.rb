c3281f2fdbc12639b4acd142c558d582b9e1649d
Masaya Takeda <844196@gmail.com>
2016-03-10 02:16:59 +0900

    インデントレベルの削減

--- app/github_user.rb
+++ app/github_user.rb
@@ -9,21 +9,20 @@ class GitHubUser
   end
 
   def public_activity
-    xml  = Nokogiri::XML.parse(open(public_feed_url)).remove_namespaces!
-    html = xml.xpath('//content').map(&:text).map do |text|
+    xml = Nokogiri::XML.parse(open(public_feed_url)).remove_namespaces!
+    xml.xpath('//content').map(&:text).map do |text|
       text.gsub!(%Q(href="/), 'href="https://github.com/')
     end
   end
 
   def contributions_calendar
-    cal = Nokogiri::HTML.parse(open(contributions_calendar_svg)).tap do |html|
-      html.css('svg.js-calendar-graph-svg').instance_eval do |svg|
-        width, height = attribute('width'), attribute('height')
-        attr('viewBox', "0 0 #{width} #{height}")
-        attr('style', "max-width: #{width}")
-        remove_attr('width')
-        remove_attr('height')
-      end
+    cal = Nokogiri::HTML.parse(open(contributions_calendar_svg))
+    cal.css('svg.js-calendar-graph-svg').instance_eval do |svg|
+      width, height = attribute('width'), attribute('height')
+      attr('viewBox', "0 0 #{width} #{height}")
+      attr('style', "max-width: #{width}")
+      remove_attr('width')
+      remove_attr('height')
     end
   end
 
